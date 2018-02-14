dceb0e4ac3dbe55c20fb234f8eabb85a2f275469
Andrés Correa Casablanca <andreu@adsmurai.com>
2018-02-14 16:35:31 +0100

    Improve the way how we handle Gradle's cache

--- 4.5-jdk8/entry_point.sh
+++ 4.5-jdk8/entry_point.sh
@@ -5,7 +5,7 @@
 # This step is done here (and not in the image building process) because the directory /home/apprunner/.gradle could   #
 # be mapped as an external volume when we want to make use of gradle's cache.                                          #
 ########################################################################################################################
-if [[ ! -f /user/.gradle/gradle.properties ]]; then
+if [[ ! -f "${GRADLE_USER_HOME}/gradle.properties" ]]; then
     echo "org.gradle.daemon=false" > /home/apprunner/.gradle/gradle.properties                                         ;
 fi
 
